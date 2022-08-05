ca46baf05c9786da76006acef6405c9cd7988671
Hubert Dulay <hubert.dulay@gmail.com>
2022-08-05 15:33:18 -0400

    tinybird

---
+++ tinybird/pipelines/wiki_flatten.sql
@@ -0,0 +1,33 @@
+insert into wikipedia_flatten
+select 
+    id,
+    title,
+    link,
+    guidislink,
+    published,
+    summary,
+    author,
+    comments,
+    diff_id,
+    oldid,
+    url_title,
+    CASE
+        WHEN LOCATE(':', url_title) > 0 THEN SPLIT_INDEX(url_title, ':', 0) 
+        ELSE null
+    END AS `type`
+from (
+    select 
+        id,
+        title,
+        link,
+        guidislink,
+        summary,
+        TO_TIMESTAMP(`published`, 'E'', ''dd'' ''MMM'' ''yyyy'' ''HH:mm:ss'' ''z') as `published`,
+        author,
+        comments,
+        PARSE_URL(id,'QUERY','diff') as diff_id,
+        PARSE_URL(id,'QUERY','oldid') as oldid,
+        PARSE_URL(id,'QUERY','title') as url_title
+    from wikipedia_raw
+    where lower(author) NOT LIKE '%bot%'
+)
---
+++ tinybird/wiki.py
@@ -0,0 +1,43 @@
+#!/bin/local/python
+
+import time
+import feedparser
+import json
+import os
+import logging
+import boto3
+from dotenv import load_dotenv
+
+
+def kinesis():
+	return boto3.client('kinesis',region_name=os.getenv("REGION"))
+
+def poll():
+	feed=feedparser.parse('https://en.wikipedia.org/w/api.php?action=feedrecentchanges&format=json')
+	entries = feed['entries']
+	stream = os.getenv("KINESIS_STREAM")
+	k = kinesis()
+
+	for e in entries:
+		print(json.dumps(e))
+		k.put_record(
+			StreamName=stream,
+			Data=json.dumps(e),
+			PartitionKey="-1")
+
+def main():
+	load_dotenv()
+	logging.basicConfig(level=logging.INFO)
+
+	while True:
+		poll()
+		time.sleep(10)
+
+	
+
+
+
+if __name__== "__main__":
+	main()
+
+	

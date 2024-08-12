83fe1edd3c3754f1e50b36489a65b19cc4548708
Sharon Xie <rxie@decodable.co>
2024-08-12 14:57:09 -0700

    sharon-pyflink-numpy-example

--- pyflink-decodable/main.py
+++ pyflink-decodable/main.py
@@ -23,15 +23,21 @@ import requests
 import jmespath
 import json
 import sys
+import numpy as np
 
 from pyflink.datastream import StreamExecutionEnvironment
 from pyflink.table import StreamTableEnvironment, DataTypes
 from pyflink.table.udf import udf
+from pyflink.common import Configuration
 
 @udf(input_types=[DataTypes.BIGINT()], result_type=DataTypes.STRING())
 def get_user_name(id):
     r = requests.get('https://jsonplaceholder.typicode.com/users/' + str(id))
     return jmespath.search("name", json.loads(r.text))
+
+@udf(input_types=[DataTypes.BIGINT()], result_type=DataTypes.ARRAY(DataTypes.INT()))
+def numpy_test(id):
+   return list(np.array([1, 2, 3, 4]))
 
 def process_todos():
     with open('/opt/pipeline-secrets/todo_kafka_user_name', 'r') as file:
@@ -46,6 +52,12 @@ def process_todos():
 
     t_env = StreamTableEnvironment.create(stream_execution_environment=env)
     t_env.create_temporary_system_function("user_name", get_user_name)
+    t_env.create_temporary_system_function("numpy_test", numpy_test)
+
+    table_config = t_env.get_config()
+    config = Configuration()
+    config.set_string("taskmanager.numberOfTaskSlots", "2")
+    table_config.add_configuration(config)
 
     kafka_jar = os.path.join(os.path.abspath(os.path.dirname(__file__)) + "/libs",
                             'flink-sql-connector-kafka-3.0.2-1.18.jar')
@@ -74,7 +86,8 @@ def process_todos():
       text      STRING,
       user_id   BIGINT,
       due       TIMESTAMP(3),
-      user_name STRING
+      user_name STRING,
+      numpy_test ARRAY<INT>
     ) WITH (
       'connector' = 'kafka',
       'topic' = 'todos',
@@ -87,7 +100,12 @@ def process_todos():
     )""")
 
     t_env.execute_sql("""
-        INSERT INTO enriched_todos SELECT *, user_name(ABS(MOD(todos.user_id, 10))) FROM todos""")
+        INSERT INTO enriched_todos 
+          SELECT 
+              *, 
+              user_name(ABS(MOD(todos.user_id, 10))),
+              numpy_test(todos.user_id)
+          FROM todos""")
 
 if __name__ == '__main__':
     logging.basicConfig(stream=sys.stdout, level=logging.INFO, format="%(message)s")

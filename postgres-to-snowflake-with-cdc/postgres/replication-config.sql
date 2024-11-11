bb8d5bf1f0d575f576a6c79526b837543c434a77
rmoff <robin@decodable.co>
2024-11-11 17:51:58 +0000

    Completed pipeline

--- postgres-to-snowflake-with-cdc/postgres/replication-config.sql
+++ postgres-to-snowflake-with-cdc/postgres/replication-config.sql
@@ -1,3 +1,6 @@
-ALTER TABLE customers REPLICA IDENTITY FULL;
-ALTER TABLE pets REPLICA IDENTITY FULL;
-ALTER TABLE appointments REPLICA IDENTITY FULL;
+ALTER TABLE omd_customers REPLICA IDENTITY FULL;
+ALTER TABLE omd_pets REPLICA IDENTITY FULL;
+ALTER TABLE omd_appointments REPLICA IDENTITY FULL;
+
+SELECT oid::regclass, relreplident FROM pg_class
+ WHERE oid in ( 'omd_customers'::regclass, 'omd_pets'::regclass, 'omd_appointments'::regclass);

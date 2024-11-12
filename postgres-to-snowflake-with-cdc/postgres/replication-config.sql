27475488ec4893258790ef0187180912f51d8359
Robin Moffatt <robin@rmoff.net>
2024-11-12 15:09:47 +0000

    comment out SELECT

--- postgres-to-snowflake-with-cdc/postgres/replication-config.sql
+++ postgres-to-snowflake-with-cdc/postgres/replication-config.sql
@@ -2,5 +2,6 @@ ALTER TABLE omd_customers REPLICA IDENTITY FULL;
 ALTER TABLE omd_pets REPLICA IDENTITY FULL;
 ALTER TABLE omd_appointments REPLICA IDENTITY FULL;
 
-SELECT oid::regclass, relreplident FROM pg_class
- WHERE oid in ( 'omd_customers'::regclass, 'omd_pets'::regclass, 'omd_appointments'::regclass);
+-- To check that this worked, run: 
+-- SELECT oid::regclass, relreplident FROM pg_class
+--  WHERE oid in ( 'omd_customers'::regclass, 'omd_pets'::regclass, 'omd_appointments'::regclass);
